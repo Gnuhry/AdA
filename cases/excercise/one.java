c5bfb271686e0585726a8e423e4cae4e9233b736
Gnuh <ytgnuh@gmail.com>
2021-01-15 11:40:48 +0100

    uncommit files

--- cases/excercise/one.java
+++ cases/excercise/one.java
@@ -1,41 +1,41 @@
-//package excercise;
-//
-//import java.util.Scanner;
-//
-//public class one {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        do {
-//            System.out.println("Taschenrechner\nWas möchten Sie tuen?\n-Addition(+)\n-Subtraktion(-)\n-Multiplikation(*)\n-Division(/)\n-Potenz(^)\n-Quadrat-Wurzel(√)");
-//            String symbol = scanner.nextLine();
-//            System.out.println("Nummer 1");
-//            int number_one = Integer.parseInt(scanner.nextLine());
-//            if (!symbol.equals("√")) {
-//                System.out.println("Nummer 2");
-//                int number_two = Integer.parseInt(scanner.nextLine());
-//            }
-//            int erg;
-//            switch (symbol) {
-//                case "+":
-//                    erg = number_one + number_two;
-//                case "-":
-//                    erg = number_one + number_two;
-//                case "*":
-//                    erg = number_one * number_two;
-//                case "/":
-//                    erg = number_one / number_two;
-//                case "^":
-//                    erg = Math.pow(number_one, number_two);
-//                case "√":
-//                    erg = Math.sqrt(number_one);
-//            }
-//            String display;
-//            if (!symbol.equals("√"))
-//                display = "Das Ergebnis von " + number_one + " " + symbol + " " + number_two + " = " + erg;
-//            else
-//                System.out.println("Das Ergebnis von " + symbol + " " + number_one + " = " + erg);
-//            System.out.println(display);
-//            System.out.println("Beenden? (1)");
-//        } while (scanner.nextLine() != "1");
-//    }
-//
+package excercise;
+
+import java.util.Scanner;
+
+public class one {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Taschenrechner\nWas möchten Sie tuen?\n-Addition(+)\n-Subtraktion(-)\n-Multiplikation(*)\n-Division(/)\n-Potenz(^)\n-Quadrat-Wurzel(√)");
+            String symbol = scanner.nextLine();
+            System.out.println("Nummer 1");
+            int number_one = Integer.parseInt(scanner.nextLine());
+            if (!symbol.equals("√")) {
+                System.out.println("Nummer 2");
+                int number_two = Integer.parseInt(scanner.nextLine());
+            }
+            int erg;
+            switch (symbol) {
+                case "+":
+                    erg = number_one + number_two;
+                case "-":
+                    erg = number_one + number_two;
+                case "*":
+                    erg = number_one * number_two;
+                case "/":
+                    erg = number_one / number_two;
+                case "^":
+                    erg = Math.pow(number_one, number_two);
+                case "√":
+                    erg = Math.sqrt(number_one);
+            }
+            String display;
+            if (!symbol.equals("√"))
+                display = "Das Ergebnis von " + number_one + " " + symbol + " " + number_two + " = " + erg;
+            else
+                System.out.println("Das Ergebnis von " + symbol + " " + number_one + " = " + erg);
+            System.out.println(display);
+            System.out.println("Beenden? (1)");
+        } while (scanner.nextLine() != "1");
+    }
+
--- cases/sy/case2.java
+++ cases/sy/case2.java
@@ -1,6 +1,6 @@
-//package error_one;
-//
-//public class case2 {
-//    public static void main(String[] args) {
-//        System.out.println("hello World");
-//    }
+package error_one;
+
+public class case2 {
+    public static void main(String[] args) {
+        System.out.println("hello World");
+    }
